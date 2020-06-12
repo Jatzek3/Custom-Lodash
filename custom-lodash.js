75dc362afbca2fd581f0efad32483f4f973ffed0
Jacek Kawalec <jokerchocolate@gmail.com>
2020-06-12 15:08:48 +0200

    feat: chunk ready

--- custom-lodash.js
+++ custom-lodash.js
@@ -1,5 +1,4 @@
-let _ = require('lodash');
-
+class CustomLodash{
 
 /*------------------------------------------------------------------------*/    
 /**
@@ -10,11 +9,43 @@ let _ = require('lodash');
  * @param {number} [size=1] The length of each chunk
  * @returns {Array} Returns the new array of chunks.
  **/
-
-    _.mixin({'customChunk' : function(arr,number) {
-        // Yet to be implemented
-        // Yet to be implemented
+    addToArray(arr, value){
+        arr[arr.length] = value;
+        return arr;
     }
-});
 
-console.log(_.customChunk([1,2,3,4],2))
+    chunk(arr, num) {
+        if (num > arr.length){
+            return arr;
+        }
+        let counter = 0;
+        let internalArrayIndex = 0;
+        let wholeArray = [[]];
+        while (arr.length > counter){
+            if (counter + arr.length % num == arr.length){
+                let lastArr = [];
+                for (let i=0; arr.length > counter; counter += 1){
+                    this.addToArray(lastArr,arr[counter])
+                }
+                _.addToArray(wholeArray, lastArr)
+                return wholeArray;
+        }
+            else if (wholeArray[internalArrayIndex].length == num){
+            this.addToArray(wholeArray, []);
+            this.addToArray[wholeArray[internalArrayIndex], arr.counter];
+            internalArrayIndex =+ 1;
+            counter +=1;
+        } else {
+            this.addToArray(wholeArray[internalArrayIndex], arr[counter]);
+            counter +=1;
+        }
+
+        };
+    }
+}
+
+let _ = new CustomLodash();
+// console.log(_.addToArray([1,2,3],4))
+console.log(_.chunk([1,2,3,4,5,6,7],3))
+
+module.exports = { _ }
--- custom-lodash.test.js
+++ custom-lodash.test.js
@@ -1,2 +1,8 @@
+import CustomLoadash from './custom-lodash.js'
+
+let _ = new CustomLoadash()
 
 
+test('split an array in to parts of given length',(_) =>{
+    expect(_.chunk([1,2,3,4,5,6,7],3)).toBe([[1,2,3],[4,5,6],[7]])
+})
