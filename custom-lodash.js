092ba0244ff686cdf5e77d8e8e4d13c0f7fa2feb
Jacek Kawalec <jokerchocolate@gmail.com>
2020-06-15 13:07:04 +0200

    feat: array methods mostly ok

--- custom-lodash.js
+++ custom-lodash.js
@@ -43,12 +43,15 @@ chunk : function(arr, num) {
 
     };
 },
+
     /**
  * Creates an array with all falsey values removed. The values `false`, `null`,
  * `0`, `""`, `undefined`, and `NaN` are falsey.
  *
  * @param {Array} array The array to compact.
- * @returns {Array} Returns the new array of filtered values.*/
+ * @returns {Array} Returns the new array of filtered values.
+ * */
+
 compact :function(arr){
     let newArr = [];
     let counter = 0
@@ -62,6 +65,7 @@ compact :function(arr){
     }
     return newArr;
 },
+
 /**
  * Creates a slice of `array` with `n` elements dropped from the beginning.
  * @param {Array} array The array to query.
@@ -77,13 +81,15 @@ drop : function(arr, num){
     }
     return newArr;
 },
+
 /**
  * Creates a slice of `array` excluding elements dropped from the beginning.
  * Elements are dropped until `predicate` returns falsey. The predicate is
  * invoked with three arguments: (value, index, array).
  * @param {Array} array The array to query.
  * @param {Function} [predicate=_.identity] The function invoked per iteration.
- * @returns {Array} Returns the slice of `array`.*/
+ * @returns {Array} Returns the slice of `array`.
+ * */
 
 dropWhile : function(arr, func){
     let newArr = [];
@@ -99,6 +105,7 @@ dropWhile : function(arr, func){
  * @param {number} [n=1] The number of elements to take.
  * @returns {Array} Returns the slice of `array`.
  * */
+
 take : function (arr, num) {
     let newArr =[];
     for (let i=0; i < num; i += 1){
@@ -110,10 +117,11 @@ take : function (arr, num) {
      * Iterates over elements of `collection`, returning an array of all elements
      * `predicate` returns truthy for. The predicate is invoked with three
      * arguments: (value, index|key, collection).
-     * @param {Array|Object} collection The collection to iterate over.
-     * @param {Function} [predicate=_.identity] The function invoked per iteration.
+     * @param {Array} collection The collection to iterate over.
+     * @param {Function} function invoked per iteration.
      * @returns {Array} Returns the new filtered array.
      * */
+
 filter : function(arr, func){
     let newArr = [];
     for(let i = 0; arr.length > i; i += 1){
@@ -123,22 +131,96 @@ filter : function(arr, func){
     }
     return newArr;
 },
-find : function(arr, func){
-    counter = 0
+    /**
+     * Iterates over elements of `collection`, returning the first element
+     * `predicate` returns truthy for. The predicate is invoked with three
+     * arguments: (value, index|key, collection).
+     * @param {Array} collection The collection to inspect.
+     * @param {Function} function invoked per iteration.
+     * @param {index} [fromIndex=0] The index to search from.
+     * @returns {*} Returns the matched element, else `undefined`.
+     **/
+
+find : function(arr, func, index = 0){
+    counter = index;
     while (arr.length > counter){
         if (func(arr[counter])){
             return arr[counter];
         }
         counter += 1;
     }
-}
+},
 
+   /**
+     * Checks if `value` is in Array If `collection`
+     *  If `fromIndex` is negative, it's used as
+     * the offset from the end of `collection`.
+     
+     * @param {Array} Array The collection to inspect.
+     * @param {*} value The value to search for.
+     * @param {number} [fromIndex=0] The index to search from.
+     * @returns {boolean} Returns `true` if `value` is found, else `false`.
+     * */
+
+includes : function(arr, value, index = 0){
+    let searchFrom  = index;
+    if (searchFrom < 0){
+        searchFrom = arr.length + index;
+    }
+    for (let i = searchFrom; arr.length > i; i+= 1){
+        if (arr[i] == value){
+            return true;
+        }
+    }
+    return false;
+},
+
+    /**
+     * Creates an array of values by running each element in Array thru
+     * function.
+     
+     * @param {Array} Array The Array to iterate over.
+     * @param {Function} [iteratee=_.identity] The function invoked per iteration.
+     * @returns {Array} Returns the new mapped array.
+     * */
+map : function(arr, func){
+    let newArr = [];
+    for (let i = 0; arr.length > i; i += 1){
+        this.addToArray(newArr, func(arr[i]))
+    }
+    return newArr;
+},
+
+    /**
+     * Creates an array of grouped elements, the first of which contains the
+     * first elements of the given arrays, the second of which contains the
+     * second elements of the given arrays, and so on.
+
+     * @param {...Array} [arrays] The arrays to process.
+     * @returns {Array} Returns the new array of grouped elements.
+     */
+zip : function(...arr){
+    let newArr = [];
+    let allArrays = arr;
+    let arrQuantity = allArrays.length;
+    for (i = 0; allArrays.length > i; i += 1){
+        this.addToArray(newArr, []);
+    }
+    for (i = 0; allArrays.length > i; i += 1){
+        for ( j = 0; allArrays.length > j; j += 1){
+            this.addToArray(newArr[j], allArrays[i][j])
+        }
+        // this.addToArray(newArr, innerArr);
+    }
+    return newArr;
+
+}
 
 }
 // console.log(CustomLodash.filter([1,2,3,4,5,6], (a) => a % 2 == 0))
 // console.log(CustomLodash.take([1,2,3,4,5,6], 3))
 // console.log(CustomLodash.dropWhile([1,2,3,4,5,6], (a)=> a < 4))
-
+console.log(CustomLodash.zip([2,4,6], ['A', 'B', 'C'], [true, false, true]))
 module.exports = { 
     CustomLodash, 
 };
--- custom-lodash.test.js
+++ custom-lodash.test.js
@@ -3,24 +3,54 @@ const CustomLodash = require('./custom-lodash.js');
 test('Adds a value to Array', () =>{
     expect(CustomLodash.CustomLodash.addToArray([1,2,3],4)).toStrictEqual([1,2,3,4]);
 });
+
 test('split an array in to parts of given length', () => {
     expect(CustomLodash.CustomLodash.chunk([1,2,3,4],3)).toStrictEqual([[1,2,3],[4]]);
 });
+
 test('Creates an array with all falsey values removed. The values `false`, `null`', () =>{
     expect(CustomLodash.CustomLodash.compact([0,1,2,3,4,null])).toStrictEqual([1,2,3,4]);
 });
+
 test('Creates a slice of `array` with `n` elements dropped from the beginning', () => {
     expect(CustomLodash.CustomLodash.drop([1,2,3,4,5,6],3)).toStrictEqual([4,5,6]);
 });
+
 // test('Creates a slice of array excluding elements dropped from the beginning. Elements are dropped until predicate returns falsey', () => {
-//     expect(CustomLodash.CustomLodash.dropWhile([1,2,3,4,5,6], (a)=> a < 4)).toStrictEqual([4,5,6])
+//     expect(CustomLodash.CustomLodash.dropWhile([1,2,3,4,5,6], (a)=> a < 4)).toContainEqual([4,5,6])
 // });
-test('Creates a slice of array with n elements taken from the beginning.', () => {
-    expect(CustomLodash.CustomLodash.take([1,2,3,4,5,6],3)).toStrictEqual([1,2,3])
+test('Creates a slice of array with n elements taken from the beginning.',
+ () => {
+    expect(CustomLodash.CustomLodash.take([1,2,3,4,5,6],3))
+    .toStrictEqual([1,2,3]);
 });
-test('Iterates over elements of collection, returning an array of all elements predicate returns truthy', () => {
-    expect(CustomLodash.CustomLodash.filter([1,2,3,4,5,6], (a) => a % 2 == 0)).toStrictEqual([2,4,6]);
+
+test('Iterates over elements of Array, returning an array of all elements predicate returns truthy',
+ () => {
+    expect(CustomLodash.CustomLodash.filter([1,2,3,4,5,6], (a) => a % 2 == 0))
+    .toStrictEqual([2,4,6]);
 });
-test('Iterates over elements of collection, returning the first element predicate returns truthy for.', () => {
-    expect(CustomLodash.CustomLodash.find([1,2,3,4,5,6], (a) => a % 3 == 0)).toBe(3);
+
+test('Iterates over elements of Array, returning the first element predicate returns truthy for.',
+ () => {
+    expect(CustomLodash.CustomLodash.find([1,2,3,4,5,6], (a) => a % 3 == 0,4))
+    .toBe(6);
 });
+
+test("Checks if value is in Array",
+ () => {
+    expect(CustomLodash.CustomLodash.includes([1,2,3,4,5], 3, -4 ))
+    .toBe(true);
+});
+
+test("Creates an array of values by running each element in Array thru iteratee.",
+ () => {
+    expect(CustomLodash.CustomLodash.map([2,4,8], (n) => n * n))
+    .toStrictEqual([4,16,64]);
+});
+
+test("Creates an array of grouped elements",
+ () => {
+    expect(CustomLodash.CustomLodash.zip([2,4,6], ['A', 'B', 'C'], [true, false, true]))
+    .toContainEqual([2,'A',true],[4,'B',false],[6,'C',true])
+})
